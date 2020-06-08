3b9978fca90e6495dae3b02887230b987df3de70
Ogboso <66277666+Ogboso@users.noreply.github.com>
2020-06-08 12:53:50 +0100

    Updated Ogboso.php

--- scripts/Ogboso.php
+++ scripts/Ogboso.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello World, this is Peace Emele with HNGi7 ID 04829 using php for stage 2 task email greatemele@gmail.com";
+echo "Hello World, this is Peace Emele with HNGi7 ID HNG-04829 using php for stage 2 task email greatemele@gmail.com";
 ?>
